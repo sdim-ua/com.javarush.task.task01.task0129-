a4836e2f3c6ef35164d1f34808335e85911616f5
sdim-ua <ingersol@yandex.ua>
2018-12-18 00:09:00 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,18 @@
+
+/* 
+Считаем длину окружности
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printCircleLength(5);
+    }
+
+    public static void printCircleLength(int radius) {
+        //напишите тут ваш код  2 * Pi * radius.
+        
+        Double Pi = 3.14;
+        System.out.println (2 * Pi * radius);
+        
+    }
+}
